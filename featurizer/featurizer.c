95fd93cf1ccf5748f2bece177fb212161fec7b37
Jure Vito Srovin <jure.vito@gmail.com>
2024-02-28 23:05:17 +0100

    Set up radial and angular values

--- featurizer/featurizer.c
+++ featurizer/featurizer.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 
+const double RADIAL_CUTOFF = 12;
+const double ANGULAR_CUTOFF = 6;
+
+const double RADIAL_STEP = 0.5;
+const double ANGULAR_STEP = 2.0;
+const int N_THETA = 8;
+
 typedef struct {
     double x;
     double y;
@@ -22,15 +29,45 @@ int square(int x) {
 }
 
 void featurize(Coord* mol_atoms, int num_mol_atom, Coord* protein_atoms, int num_protein_atom) {
-    int** dist = (int**)malloc(num_mol_atom*sizeof(int*));
+
+    // Setting radial steps.
+    int rs_radial_length = ceil((RADIAL_CUTOFF - 0.5) / RADIAL_STEP);
+    double* rs_radial = (double*)malloc(rs_radial_length*sizeof(double));
+
+    double rad = 0.5;
+    for (int i = 0; i<rs_radial_length; i++) {
+        rs_radial[i] = rad;
+        rad += RADIAL_STEP;
+    }
+
+    // Setting angular steps.
+    int rs_angular_length = ceil((ANGULAR_CUTOFF - 0.5) / ANGULAR_STEP);
+    double* rs_angular = (double*)malloc(rs_angular_length*sizeof(double));
+
+    double ang = 0.5;
+    for (int i = 0; i<rs_angular_length; i++) {
+        rs_angular[i] = ang;
+        ang += ANGULAR_STEP;
+    }
+
+    // Setting theta list.
+    double* theta_list = (double*)malloc(N_THETA*sizeof(double));
+    double interval = 2*M_PI / (N_THETA);
+    for (int i = 0; i < N_THETA; i++) {
+        theta_list[i] = interval*i;
+    }
+
+
+    double** dist = (double**)malloc(num_mol_atom*sizeof(double*));
     for (int i = 0; i < num_mol_atom; i++) {
-        dist[i] = (int*)malloc(num_protein_atom*sizeof(int));
+        dist[i] = (double*)malloc(num_protein_atom*sizeof(double));
     }
 
     for (int i = 0; i < num_mol_atom; i++) {
         for (int j = 0; j < num_protein_atom; j++) {
 			dist[i][j] = eucl_dist(mol_atoms[i], protein_atoms[j]);
 		}
+        break;
     }
 
 	printf("Matrix has %d element\n", num_mol_atom*num_protein_atom);
