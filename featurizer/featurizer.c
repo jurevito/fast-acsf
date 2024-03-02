41f546a9d0aaa6a9a316fb5782937b205fbefe4c
Jure Vito Srovin <jure.vito@gmail.com>
2024-03-02 11:02:00 +0100

    Change variable names and move some blocks around

--- featurizer/featurizer.c
+++ featurizer/featurizer.c
@@ -29,17 +29,18 @@ double angular_sym_func(double Rij, double Rik, double theta_ijk, double theta, 
     return pow(2, 1-zeta) * pow(1 + cos(theta_ijk - theta), zeta) * exp(-eta * pow(((Rij + Rik) / 2 - Rs), 2)) * fc_Rij * fc_Rik;
 }
 
-int find_angular_index(int lig_atom_idx, int p1_atom_idx, int p2_atom_idx, int th, int rs, int rs_angular_length) {
+int angular_index(int p1_atom_idx, int lig_atom_idx, int p2_atom_idx, int th, int rs, int rs_angular_length) {
     if (p1_atom_idx > p2_atom_idx) {
         int tmp = p1_atom_idx;
         p1_atom_idx = p2_atom_idx;
         p2_atom_idx = tmp;
     }
     
-    int part1 = (N_ELEMENTS*(N_ELEMENTS+1) / 2 * lig_atom_idx);
-    int part2 = (p1_atom_idx / 2.0f * (2 * N_ELEMENTS - p1_atom_idx + 1));
-    int part3 = (p2_atom_idx-p1_atom_idx);
-    return N_THETA*rs_angular_length*(part1 + part2 + part3) + th*rs_angular_length + rs;
+    int index = (N_ELEMENTS*(N_ELEMENTS+1) / 2 * lig_atom_idx);
+    index += (p1_atom_idx / 2.0f * (2 * N_ELEMENTS - p1_atom_idx + 1));
+    index += (p2_atom_idx-p1_atom_idx);
+    index *= N_THETA*rs_angular_length;
+    return index + th*rs_angular_length + rs;
 }
 
 double* featurize(Atom* mol_atoms, int num_mol_atom, Atom* protein_atoms, int num_protein_atom) {
@@ -64,17 +65,10 @@ double* featurize(Atom* mol_atoms, int num_mol_atom, Atom* protein_atoms, int nu
         ang += ANGULAR_STEP;
     }
 
-    // Setting theta list.
-    double* theta_list = malloc(N_THETA*sizeof(double));
-    double interval = 2*M_PI / (N_THETA);
-    for (int i = 0; i < N_THETA; i++) {
-        theta_list[i] = interval*i;
-    }
-
     //Setting PL distance matrix.
-    double** dist_lp = malloc(num_mol_atom*sizeof(double*));
+    double** dist = malloc(num_mol_atom*sizeof(double*));
     for (int i = 0; i < num_mol_atom; i++) {
-        dist_lp[i] = malloc(num_protein_atom*sizeof(double));
+        dist[i] = malloc(num_protein_atom*sizeof(double));
     }
 
     // Setting result vector.
@@ -84,26 +78,33 @@ double* featurize(Atom* mol_atoms, int num_mol_atom, Atom* protein_atoms, int nu
 
     for (int i = 0; i < num_mol_atom; i++) {
         for (int j = 0; j < num_protein_atom; j++) {
-			dist_lp[i][j] = eucl_dist(mol_atoms[i], protein_atoms[j]);
+			dist[i][j] = eucl_dist(mol_atoms[i], protein_atoms[j]);
 
             // Calculate features only for pairs within radius.
-            if (dist_lp[i][j] < RADIAL_CUTOFF) {
+            if (dist[i][j] < RADIAL_CUTOFF) {
                 
                 // Calculate radial features.
                 for (int k = 0; k<rs_radial_length; k++) {
                     int index = (rs_radial_length*N_ELEMENTS*mol_atoms[i].atom_index) + (rs_radial_length*protein_atoms[j].atom_index) + k;
-                    result[index] += radial_sym_func(dist_lp[i][j], rs_radial[k]);
+                    result[index] += radial_sym_func(dist[i][j], rs_radial[k]);
                 }
             }
 		}
     }
 
+    // Setting theta list.
+    double* theta_list = malloc(N_THETA*sizeof(double));
+    double interval = 2*M_PI / (N_THETA);
+    for (int i = 0; i < N_THETA; i++) {
+        theta_list[i] = interval*i;
+    }
+
     // Search for triplets of atoms close by to each other.
     for (int lig_idx = 0; lig_idx < num_mol_atom; lig_idx++) {
         for (int p1_idx = 0; p1_idx < num_protein_atom; p1_idx++) {
-            if (dist_lp[lig_idx][p1_idx] < ANGULAR_CUTOFF) {
+            if (dist[lig_idx][p1_idx] < ANGULAR_CUTOFF) {
                 for (int p2_idx = p1_idx+1; p2_idx<num_protein_atom; p2_idx++) { 
-                    if (dist_lp[lig_idx][p2_idx] < ANGULAR_CUTOFF) {
+                    if (dist[lig_idx][p2_idx] < ANGULAR_CUTOFF) {
                         
                         int lig_atom_idx = mol_atoms[lig_idx].atom_index;
                         int p1_atom_idx = protein_atoms[p1_idx].atom_index;
@@ -113,8 +114,8 @@ double* featurize(Atom* mol_atoms, int num_mol_atom, Atom* protein_atoms, int nu
                         double angle = calc_angle(mol_atoms[lig_idx], protein_atoms[p1_idx], protein_atoms[p2_idx]);
                         for (int l = 0 ; l<rs_angular_length ; l++) {
                             for (int m = 0 ; m<N_THETA; m++) {
-                                int index = find_angular_index(lig_atom_idx, p1_atom_idx, p2_atom_idx, m, l, rs_angular_length);
-                                result[index + radial_length] += angular_sym_func(dist_lp[lig_idx][p1_idx], dist_lp[lig_idx][p2_idx], angle, theta_list[m], rs_angular[l]);
+                                int index = angular_index(p1_atom_idx, lig_atom_idx, p2_atom_idx, m, l, rs_angular_length);
+                                result[index + radial_length] += angular_sym_func(dist[lig_idx][p1_idx], dist[lig_idx][p2_idx], angle, theta_list[m], rs_angular[l]);
                             }
                         }
                     }
@@ -131,9 +132,9 @@ double* featurize(Atom* mol_atoms, int num_mol_atom, Atom* protein_atoms, int nu
     free(rs_angular);
     free(theta_list);
     for (int i = 0; i < num_mol_atom; i++) {
-        free(dist_lp[i]);
+        free(dist[i]);
     }
-    free(dist_lp);
+    free(dist);
 
     return result;
 }
